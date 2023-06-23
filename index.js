7712c43e31b21172462981dadb93c69895e5b657
JahongirObloqulov <jahongir.obloqulov13@gmail.com>
2023-06-23 18:50:46 +0500

    javascript qismi qisman yakullandi

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-let btn = document.querySelector('.btn');
-btn.addEventListener('click', () => {
-    console.log("salom");
-})
+let text = document.querySelector('.text');
+let numberInput = document.getElementById('num');
+let formGroup = document.querySelector('.form-group');
+let links = document.querySelectorAll('a');
+formGroup.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (numberInput.value === "1") {
+        text.textContent = 'Positive content';
+    } else if (numberInput.value === "2") {
+        text.textContent = 'Negative content';
+    } else if (numberInput.value === "3") {
+        text.textContent = 'Neutral content';
+    } else {
+        alert("Index is invalid");
+    }
+    numberInput.value = "";
+
+});
+
+
+
+
+
+
