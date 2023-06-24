d2fb7013ead00d9c31795758092b31b992da7c69
JahongirObloqulov <jahongir.obloqulov13@gmail.com>
2023-06-24 05:11:18 +0500

    Dastur yakullandi

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-let text = document.querySelector('.text');
-let numberInput = document.getElementById('num');
-let formGroup = document.querySelector('.form-group');
-let links = document.querySelectorAll('a');
-formGroup.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (numberInput.value === "1") {
-        text.textContent = 'Positive content';
-    } else if (numberInput.value === "2") {
-        text.textContent = 'Negative content';
-    } else if (numberInput.value === "3") {
-        text.textContent = 'Neutral content';
-    } else {
-        alert("Index is invalid");
+let text = document.querySelector(".text");
+let numberInput = document.getElementById("num");
+let links = document.querySelectorAll(".link");
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  if (numberInput.value <= 3) {
+    const index = numberInput.value - 1;
+    if (index >= 0 && index < links.length) {
+      for (let i = 0; i < links.length; i++) {
+        links[i].classList.remove("active");
+      }
+      links[index].classList.add("active");
+      text.textContent = `${links[index].textContent} content`;
+      numberInput.value = "";
     }
-    numberInput.value = "";
-
+  }
 });
 
-
-
-
-
-
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", () => {
+    for (j = 0; j < links.length; j++) {
+      links[j].classList.remove("active");
+    }
+    links[i].classList.add("active");
+    text.textContent = `${links[i].textContent} content`;
+  });
+}
